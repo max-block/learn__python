2589996af12539cb7274e00a3983a5102800e346
Max Block <maxblock@pm.me>
2020-10-20 09:13:08 +0300

    update

--- learn__subprocess.py
+++ learn__subprocess.py
@@ -4,15 +4,18 @@ import subprocess
 
 def f1():
     cmd = "ls -l /"
-    process = subprocess.run(shlex.split(cmd), timeout=30)
-    print(process.stdout)
+    process = subprocess.run(shlex.split(cmd), timeout=30, capture_output=True)
+    out = process.stdout.decode("utf-8")
+    print(out)
 
 
 def exec_command_on_remote_host(ssh_file, host, command):
     cmd = f"ssh -i {ssh_file} {host} '{command}'"
-    process = subprocess.run(shlex.split(cmd), timeout=30)
-    print(process.stdout)
+    process = subprocess.run(shlex.split(cmd), timeout=30, capture_output=True)
+    out = process.stdout.decode("utf-8")
+    print(out)
 
 
 if __name__ == "__main__":
-    exec_command_on_remote_host("~/key1", "test", "top")
+    f1()
+    # exec_command_on_remote_host("~/key1", "test", "top")
